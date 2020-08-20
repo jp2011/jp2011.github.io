796dc482b9234598800349dc26d63394d343b962
Jan Povala <jan.povala11@imperial.ac.uk>
2020-08-20 17:17:26 +0100

    rebuilding site Thu 20 Aug 2020 17:17:26 BST

---
+++ publication/2018-lgcp-process-london-crime-mres-thesis/auto/cite.el
@@ -0,0 +1,7 @@
+(TeX-add-style-hook
+ "cite"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "povalaUnderstandingLondonCrime2018"))
+ :bibtex)
+
---
+++ publication/2020_spatial_poisson_mixtures/auto/cite.el
@@ -0,0 +1,7 @@
+(TeX-add-style-hook
+ "cite"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "povalaBurglaryLondonInsights2020a"))
+ :bibtex)
+
